52db3024a3159c0a764194739ef847f2a4833ddf
李书圣 <2084447140@qq.com>
2022-09-09 10:51:50 +0800

    init

---
+++ src/main/java/com/lsstop/config/RpcAutoConfiguration.java
@@ -0,0 +1,87 @@
+package com.lsstop.config;
+
+import com.lsstop.entity.URL;
+import com.lsstop.enums.RpcErrorEnum;
+import com.lsstop.exception.RpcException;
+import com.lsstop.loadbalancer.LoadBalance;
+import com.lsstop.properties.RpcConfigProperties;
+import com.lsstop.proxy.RpcClientProxy;
+import com.lsstop.registry.Consul.ConsulRegistry;
+import com.lsstop.registry.Nacos.NacosRegistry;
+import com.lsstop.registry.Redis.RedisRegistry;
+import com.lsstop.registry.RegistryCenter;
+import com.lsstop.serializable.CommonSerializer;
+import com.lsstop.serializable.FastJsonSerializer;
+import com.lsstop.serializable.JsonSerializer;
+import com.lsstop.spring.SpringBeanPostProcessor;
+import com.lsstop.transport.netty.client.NettyClient;
+import com.lsstop.transport.netty.server.NettyServer;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @author lss
+ * @date 2022/09/07
+ */
+@Configuration
+@EnableConfigurationProperties(RpcConfigProperties.class)
+@ConditionalOnProperty(prefix = "spring.rpc", value = "enabled", matchIfMissing = true)
+public class RpcAutoConfiguration implements ApplicationRunner {
+
+    @Resource
+    private RpcConfigProperties configProperties;
+
+    @Bean
+    public SpringBeanPostProcessor springBeanPostProcessor() {
+        return new SpringBeanPostProcessor(new RpcClientProxy(nettyClient()));
+    }
+
+    public NettyClient nettyClient() {
+        if (configProperties.getRegistryAddress() == null) {
+            throw new RpcException("未设置注册中心地址: registryAddress");
+        }
+        LoadBalance balance = LoadBalance.getBalance(configProperties.getLoadbalancer());
+        String[] split = configProperties.getRegistryAddress().toLowerCase().split("://");
+        RegistryCenter center = null;
+        if ("nacos".equals(split[0])) {
+            center = new NacosRegistry(balance);
+        } else if ("consul".equals(split[0])) {
+            center = new ConsulRegistry(balance);
+        } else if ("redis".equals(split[0])) {
+            center = new RedisRegistry(balance);
+        } else {
+            throw new RpcException("注册中心设置错误");
+        }
+        CommonSerializer serializer = null;
+        if ("fastjson".equals(configProperties.getSerializer())) {
+            serializer = new FastJsonSerializer();
+        } else if ("jackson".equals(configProperties.getSerializer())) {
+            serializer = new JsonSerializer();
+        } else {
+            throw new RpcException("序列化方式设置错误: serializer");
+        }
+        if (configProperties.getRegistry()) {
+            if (configProperties.getName() == null || configProperties.getName().trim().isEmpty()) {
+                throw new RpcException(RpcErrorEnum.NOT_SETUP_SERVICE);
+            }
+            URL url = new URL(configProperties.getName(), configProperties.getHost(), configProperties.getPort(), configProperties.getWeight());
+            center.registered(configProperties.getName(), url);
+        }
+        return new NettyClient(center, serializer);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        //是否注册服务
+        if (configProperties.getRegistry()) {
+            NettyServer server = new NettyServer();
+            server.start(configProperties.getHost(), configProperties.getPort());
+        }
+    }
+}
---
+++ src/main/java/com/lsstop/properties/RpcConfigProperties.java
@@ -0,0 +1,117 @@
+package com.lsstop.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lss
+ * @date 2022/09/07
+ */
+@Component
+@ConfigurationProperties(prefix = "spring.rpc")
+public class RpcConfigProperties {
+
+    /**
+     * 注册中心地址
+     */
+    private String registryAddress;
+
+    /**
+     * 注册名称
+     */
+    private String name;
+
+    /**
+     * ip
+     */
+    private String host = "127.0.0.1";
+
+    /**
+     * 端口
+     */
+    private int port = 8080;
+
+    /**
+     * 权重
+     */
+    private Integer weight = 1;
+
+    /**
+     * 序列化方式
+     */
+    private String serializer = "fastjson";
+
+    /**
+     * 负载均衡算法
+     */
+    private String loadbalancer = "weightRandom";
+
+    /**
+     * 是否注册服务
+     */
+    private Boolean registry = true;
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getRegistry() {
+        return registry;
+    }
+
+    public void setRegistry(Boolean registry) {
+        this.registry = registry;
+    }
+
+    public String getRegistryAddress() {
+        return registryAddress;
+    }
+
+    public void setRegistryAddress(String registryAddress) {
+        this.registryAddress = registryAddress;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getSerializer() {
+        return serializer;
+    }
+
+    public void setSerializer(String serializer) {
+        this.serializer = serializer;
+    }
+
+    public String getLoadbalancer() {
+        return loadbalancer;
+    }
+
+    public void setLoadbalancer(String loadbalancer) {
+        this.loadbalancer = loadbalancer;
+    }
+}
